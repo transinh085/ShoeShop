3ae5f4837c8b50e63458f953bf88d43035f580c8
Trần Nhật Sinh <45101901+transinh085@users.noreply.github.com>
2023-11-01 16:52:37 +0700

    Add authorize route

--- Areas/Admin/Controllers/BrandsController.cs
+++ Areas/Admin/Controllers/BrandsController.cs
@@ -1,11 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ShoeShop.Data;
 using ShoeShop.Models;
 
 namespace ShoeShop.Areas.Admin.Controllers
 {
-    [Area("Admin")]
+	[Authorize]
+	[Area("Admin")]
     public class BrandsController : Controller
     {
         private readonly AppDbContext _context;
