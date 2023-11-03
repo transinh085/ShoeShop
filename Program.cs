c44e7d2c02c73190dcbb32b9b69e615bc2e19447
Trần Nhật Sinh <45101901+transinh085@users.noreply.github.com>
2023-11-03 11:20:34 +0700

    Load data create product

--- Program.cs
+++ Program.cs
@@ -7,11 +7,7 @@ using ShoeShop.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews().AddRazorOptions(options =>
-{
-	//options.ViewLocationFormats.Add("/Components/ShoeCards/Default.cshtml");
-});
-;
+builder.Services.AddControllersWithViews();
 
 
 builder.Services.AddDbContext<AppDbContext>(options =>
@@ -25,7 +21,6 @@ builder.Services.AddMemoryCache();
 builder.Services.AddSession();
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
 	.AddCookie();
-//builder.Services.AddSingleton<ViewComponent, ShoeCardsViewComponent>();
 
 builder.Services.ConfigureApplicationCookie(options =>
 {
--- wwwroot/admin/js/upload-image.js
+++ wwwroot/admin/js/upload-image.js
@@ -10,32 +10,50 @@ uploadContainer.addEventListener("click", () => {
 
 uploadImgInput.addEventListener("change", (e) => {
     let fileInput = uploadImgInput.files;
-    console.info(fileInput);
 
     for (let i = 0; i < fileInput.length; i++) {
-        files.push(fileInput[i]);
+        if (files.every((item) => item.name != fileInput[i].name)) {
+            files.push(fileInput[i]);
+        }
     }
 
     showImage();
 });
 
 const showImage = () => {
+    checkUI();
     let images = '';
+    files.forEach(function (file, index) {
+        images += `<div class="preview-image-item">
+                     <img src="${URL.createObjectURL(file)}" alt="image" />
+                        <span class="btn-delete-image" onClick="handleDelete(event,${index}, this.parentElement)"><i class="fa fa-fw fa-times"></i></span></div>`
+    });
+    hasImage.innerHTML = images;
+}
+
+const handleDelete = (event, index, parentElement) => {
+    event.stopPropagation();
+    files.splice(index, 1);
+    checkUI();
+    parentElement.parentNode.removeChild(parentElement);
+}
+
+const checkUI = () => {
     if (files.length > 0) {
         noImage.style.display = "none";
     } else {
         noImage.style.display = "block";
     }
-    files.forEach(function (file, index) {
-        images += `<div class="preview-image-item">
-                     <img src="${URL.createObjectURL(file)}" alt="image" />
-                        <span class="btn-delete-image" onClick="handleDelete(event,${index})"><i class="fa fa-fw fa-times"></i></span></div>`
-    });
-    hasImage.innerHTML = images;
 }
 
-const handleDelete = (event, index) => {
-    event.stopPropagation();
-    files.splice(index, 1);
-    showImage();
-}
+// Handle change variant 
+let colorEl = $("#color-id");
+let sizeEl = $("#size-id");
+
+colorEl.on("change.select2", function (e) {
+    console.info("colorEl change");
+});
+
+sizeEl.on("change.select2", function (e) {
+    console.info("sizeEl change");
+});
