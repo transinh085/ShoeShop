66fa1efd59bae8160fe37e0dfcf134e32e3135c9
Trần Nhật Sinh <45101901+transinh085@users.noreply.github.com>
2023-11-04 17:18:13 +0700

    backup

--- Areas/Admin/Controllers/ProductsController.cs
+++ Areas/Admin/Controllers/ProductsController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ShoeShop.Data;
+using ShoeShop.ViewModels.Product;
+using System.Diagnostics;
 
 namespace ShoeShop.Areas.Admin.Controllers
 {
@@ -8,10 +10,12 @@ namespace ShoeShop.Areas.Admin.Controllers
     public class ProductsController : Controller
 	{
         private readonly AppDbContext _context;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public ProductsController(AppDbContext context)
+        public ProductsController(AppDbContext context, IWebHostEnvironment webHostEnvironment)
         {
             _context = context;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public IActionResult Index()
@@ -31,5 +35,50 @@ namespace ShoeShop.Areas.Admin.Controllers
             ViewBag.Sizes = sizes;
             return View();
 		}
-	}
+
+        [HttpPost]
+        [Consumes("multipart/form-data")]
+        public IActionResult Create([FromForm] ProductViewModel productViewModel)
+        {
+            string? productName = productViewModel.Name;
+            Console.WriteLine(productName);
+            decimal productPrice = productViewModel.Price;
+            string productDescription = productViewModel.Description;
+            bool productStatus = productViewModel.Status;
+            string productSlug = productViewModel.Slug;
+            string productCategory = productViewModel.Category;
+            string productBrand = productViewModel.Brand;
+
+            foreach (var variant in productViewModel.Variants)
+            {
+                string colorId = variant.ColorId;
+
+                foreach (var size in variant.Sizes)
+                {
+                    int sizeId = size.Id;
+                    int sizeStock = size.Stock;
+                    bool sizeActive = size.Active;
+                }
+
+                foreach (var image in variant.Images)
+                {
+                    if (image.Length > 0)
+                    {
+                        string uniqueFileName = Guid.NewGuid().ToString() + "_" + image.FileName;
+                        string filePath = Path.Combine("wwwroot/test", uniqueFileName);
+
+                        using (var fileStream = new FileStream(filePath, FileMode.Create))
+                        {
+                            image.CopyTo(fileStream);
+                        }
+                        Console.WriteLine(filePath);
+                    }
+                }
+            }
+
+            // Xử lý dữ liệu sản phẩm và hình ảnh theo nhu cầu của bạn
+
+            return Ok(new { message = "Dữ liệu sản phẩm đã được xử lý thành công!" });
+        }
+    }
 }
--- wwwroot/admin/js/upload-image.js
+++ wwwroot/admin/js/upload-image.js
@@ -12,7 +12,6 @@ uploadContainer.addEventListener("click", () => {
 
 uploadImgInput.addEventListener("change", (e) => {
   let fileInput = uploadImgInput.files;
-    console.info(fileInput);
   for (let i = 0; i < fileInput.length; i++) {
     if (files.every((item) => item.name != fileInput[i].name)) {
       files.push(fileInput[i]);
@@ -68,16 +67,16 @@ colorEl.on("change.select2", function (e) {
 });
 
 sizeEl.on("change.select2", function (e) {
-    showVariantSize($("#color-id option:selected").text(), sizeEl.val());
+    showVariantSize(sizeEl.val());
 });
 
-const showVariantSize = (color, sizes) => {
+const showVariantSize = (sizes) => {
   let html = "";
   sizes.forEach(function (item, index) {
       let sizeName = $(`#size-id option[value='${item}']`).text();
       html += `<tr class="row-size" data-id="${item}" data-name="${sizeName}">
-    <th scope="row">${index++}</th>
-    <td>${color + " / " + sizeName}</td>
+    <th scope="row">${++index}</th>
+    <td>${sizeName}</td>
     <td>
         <input type="number" class="form-control form-control-sm w-50 input-stock">
     </td>
@@ -96,13 +95,7 @@ const showVariantSize = (color, sizes) => {
 };
 
 $("#btn-add-variant").click(() => {
-    const variant = {
-        color_id: colorEl.val(),
-        color_name: $("#color-id option:selected").text(),
-        sizes: getSize(),
-        images: files,
-        thumbnail: $(".preview-image-item img.active").data("id")
-    };
+    const variant = getObjVariant();
     variants.push(variant);
     showVariant();
     resetDialogVariant();
@@ -127,37 +120,171 @@ const getSize = () => {
 
 const resetDialogVariant = () => { 
     files = [];
-    showImage();
+    showImage(files);
     colorEl.val([]).trigger('change');
     sizeEl.val([]).trigger('change');
 }
 
 const showVariant = () => {
-    let html = '';
-    variants.forEach((variant, index) => {
-        const { color_id, color_name, images, sizes, thumbnail } = variant;
+    const htmlArray = variants.map((variant, index) => {
+        const { colorName, sizes, thumbnail } = variant;
         const textSize = sizes.map(item => item.name).join(", ");
-        html += `<tr><th>
-          <img class="img-variant" src="${URL.createObjectURL(images[thumbnail ?? 0])}" alt="" />
+        const imageUrl = URL.createObjectURL(variant.images[thumbnail ?? 0]);
+        return `<tr><th>
+            <img class="img-variant" src="${imageUrl}" alt="" />
         </th>
-         <td>${color_name}</td>
-         <td>${textSize}</td>
-         <td class="text-center">
-         <div class="btn-group">
-             <button data-index="${index}" type="button" class="btn-edit-variant btn btn-sm btn-alt-secondary" data-bs-toggle="tooltip" title="Edit">
+        <td>${colorName}</td>
+        <td>${textSize}</td>
+        <td class="text-center">
+            <div class="btn-group">
+                <button data-index="${index}" type="button" class="btn-up-variant btn btn-sm btn-alt-secondary">
+                    <i class="fa fa-arrow-up"></i>
+                </button>
+                <button data-index="${index}" type="button" class="btn-down-variant btn btn-sm btn-alt-secondary">
+                    <i class="fa fa-arrow-down"></i>
+                </button>
+                <button data-index="${index}" type="button" class="btn-edit-variant btn btn-sm btn-alt-secondary">
                     <i class="fa fa-pencil-alt"></i>
-             </button>
-             <button data-index="${index}" type="button" class="btn-delete-variant btn btn-sm btn-alt-secondary" data-bs-toggle="tooltip" title="Delete">
+                </button>
+                <button data-index="${index}" type="button" class="btn-delete-variant btn btn-sm btn-alt-secondary">
                     <i class="fa fa-times"></i>
-             </button>
-         </div>
+                </button>
+            </div>
         </td></tr>`;
-    })
-    $("#variant-color").html(html);
-}
+    });
 
-$(document).on("click", ".btn-delete-variant", function () {
-    let index = $(this).data("index");
+    const newHtml = htmlArray.join('');
+    const variantColorContainer = $("#variant-color");
+    if (newHtml !== variantColorContainer.html()) {
+        variantColorContainer.html(newHtml);
+    }
+
+    if (variants.length > 0) {
+        $(".variants-container").show();
+    } else {
+        $(".variants-container").hide();
+    }
+};
+
+$(".variants-container").on("click", ".btn-edit-variant", function (event) {
+    const index = $(this).data("index");
+
+    $("#btn-add-variant").hide();
+    $("#btn-update-variant").show();
+    $("#btn-update-variant").data("index", index);
+
+    showModalVariant(variants[index]);
+    $("#modal-variant").modal('show');
+});
+
+$(".variants-container").on("click", ".btn-delete-variant", function (event) {
+    const index = $(this).data("index");
     variants.splice(index, 1);
     showVariant()
 });
+
+$("#btn-save-product").click(() => {
+    const productData = {
+        Name: $('#product-name').val(),
+        Price: $('#product-price').val(),
+        Description: CKEDITOR.instances['js-ckeditor'].getData(),
+        Status: $('#product-status').prop("checked"),
+        Slug: $('#product-slug').val(),
+        Category: $('#category-id').val(),
+        Brand: $('#brand-id').val(),
+        Variants: variants 
+    };
+
+    console.info(productData);
+
+    var formData = new FormData();
+
+    // Thêm các thuộc tính của sản phẩm vào FormData
+    formData.append('Name', productData.Name);
+    formData.append('Price', productData.Price);
+    formData.append('Description', productData.Description);
+    formData.append('Status', productData.Status);
+    formData.append('Slug', productData.Slug);
+    formData.append('Category', productData.Category);
+    formData.append('Brand', productData.Brand);
+
+    // Thêm các biến thể của sản phẩm
+    productData.Variants.forEach((variant, variantIndex) => {
+        formData.append(`Variants[${variantIndex}].ColorId`, variant.colorId);
+        formData.append(`Variants[${variantIndex}].ColorName`, variant.colorName);
+
+        console.info(variant.images);
+
+        // Thêm các hình ảnh blob cho biến thể hiện tại
+        variant.images.forEach((imageBlob, imageIndex) => {
+            formData.append(`Variants[${variantIndex}].Images`, imageBlob, imageBlob.name);
+        });
+
+        // Thêm các kích thước cho biến thể hiện tại
+        variant.sizes.forEach((size, sizeIndex) => {
+            formData.append(`Variants[${variantIndex}].Sizes[${sizeIndex}].Id`, size.id);
+            formData.append(`Variants[${variantIndex}].Sizes[${sizeIndex}].Name`, size.name);
+            formData.append(`Variants[${variantIndex}].Sizes[${sizeIndex}].Stock`, size.stock);
+            formData.append(`Variants[${variantIndex}].Sizes[${sizeIndex}].Active`, size.active);
+        });
+    });
+
+    console.log(formData);
+
+    $.ajax({
+        type: 'POST',
+        url: '/Admin/Products/Create',
+        data: formData,
+        processData: false,
+        contentType: false,
+        success: function (response) {
+            console.log(response);
+        },
+        error: function (error) {
+            console.error(error);
+        }
+    });
+})
+
+const showModalVariant = (variant) => {
+    const { colorId, sizes, images, thumbnail } = variant;
+    const arrSize = sizes.map(item => item.id);
+    files = images;
+    showImage();
+    $(`.preview-image-item img[data-id='${thumbnail}']`).addClass('active');
+    colorEl.val(colorId).trigger('change');
+    sizeEl.val(arrSize).trigger('change');
+    setStockAndActive(sizes);
+}
+
+const setStockAndActive = (sizes) => {
+    let sizesRow = $(".row-size");
+
+    sizesRow.each(function (index) {
+        const { stock, active } = sizes[index];
+        $(this).find(".input-stock").val(stock);
+        $(this).find(".input-active").prop("checked", active);
+    });
+}
+
+$("#btn-update-variant").click(() => {
+    const index = $(this).data("index");
+    const variant = getObjVariant();
+    variants[index] = variant;
+    showVariant();
+    resetDialogVariant();
+    $("#btn-add-variant").show();
+    $("#btn-update-variant").hide();
+})
+
+const getObjVariant = () => {
+    const variant = {
+        colorId: colorEl.val(),
+        colorName: $("#color-id option:selected").text(),
+        sizes: getSize(),
+        images: files,
+        thumbnail: $(".preview-image-item img.active").data("id")
+    };
+    console.info(variant);
+    return variant;
+}
