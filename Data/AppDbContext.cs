872daed30c2591d0e15f7c231ba3011ab8e6db70
Trần Nhật Sinh <45101901+transinh085@users.noreply.github.com>
2023-11-16 08:08:34 +0700

    fix merge

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -6,17 +6,22 @@ namespace ShoeShop.Data
 {
 	public class AppDbContext : IdentityDbContext<AppUser>
 	{
-		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-		public DbSet<Category> Categories { get; set; }
-		public DbSet<Color> Colors { get; set; }
-		public DbSet<Brand> Brands { get; set; }
-		public DbSet<Size> Sizes { get; set; }
-		public DbSet<Product> Products { get; set; }
-		public DbSet<Variant> Variants { get; set; }
-		public DbSet<Image> Images { get; set; }
-		public DbSet<VariantSize> VariantSizes { get; set; }
-		public DbSet<Topic> Topics { get; set; }
-		public DbSet<Blog> Blogs { get; set; }
-		public DbSet<Contact> Contacts {  get; set; }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Color> Colors { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Size> Sizes { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Variant> Variants { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<VariantSize> VariantSizes { get; set; }
+        public DbSet<Topic> Topics { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<ShippingMethod>? ShippingMethods { get; set; }
+        public DbSet<Address>? Addresses { get; set; }
+        public DbSet<Order>? Orders { get; set; }
+        public DbSet<OrderDetail>? OrderDetails { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
     }
 }
