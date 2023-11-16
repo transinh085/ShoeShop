87dd89ab53b25d5d043b549d0e1308b5c0e2a132
Hoàng Gia Bảo <120194990+hgbaodev@users.noreply.github.com>
2023-11-16 08:09:58 +0700

    add account controller

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShoeShop.Controllers
+{
+	public class AccountController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
