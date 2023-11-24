0d8bf089b22698e69101da56bc0999664c1ce154
Hoàng Gia Bảo <120194990+hgbaodev@users.noreply.github.com>
2023-11-24 23:11:48 +0700

    refactor bs5

--- Program.cs
+++ Program.cs
@@ -9,7 +9,6 @@ using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddSignalR();
 
@@ -47,7 +46,6 @@ if (args.Length == 1 && args[0].ToLower() == "seed")
 	Seed.SeedData(app);
 }
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
